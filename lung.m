eea2a8bc83bbaa61cc04a4e7d3aa1408db2f7699
jennLiang1 <lliangyueshan@gmail.com>
2023-04-23 18:53:23 -0700

    check tasks 1-10

--- lung.m
+++ lung.m
@@ -46,6 +46,7 @@ global Pstar cstar n maxcount M Q camax RT cI;
 
 % run with M values set below and comment out deafult M = 0.25*cref*5.6
 cref=0.2/(22.4*(310/273));
+beta = 0.2;
 
 for i = 1:100
     disp("Iteration: "+ i)
@@ -64,33 +65,33 @@ fprintf("The maximum sustainable rate of oxygen consumption for the given set of
 % Task 4 part 2: Find M for different beta; 
 % comment out default beta = 0.5 and M = 0.25*cref*5.6
 
-clear all
-clf
-global Pstar cstar n maxcount M Q camax RT cI;
-
-% for manually modifying beta = 0:0.1:1
-beta = 1;
-
-for i = 1:100
-    disp("Iteration: "+ i)
-    M = (0.25*cref*5.6)*i;
-    
-    setup_lung
-    cvsolve
-    outchecklung
-end
-
-% by modifying beta = 0:0.1:1, 
-% I found out that when beta=1:0.1:0.7, M_max = 0.0330 
-% and when beta = 0.8:1, M_max = 0.0220
-M_max = [repelem(0.0330, 8), repelem(0.0220, 3)];
-beta = 0:0.1:1;
-figure(5)
-plot(beta, M_max, 'o')
-hold on
-plot(beta, M_max, 'g')
-xlabel("beta"); ylabel("maximum sustainable rate of oxygen consumption"); 
-title("beta vs max sustainable rate of oxygen consumption")
+% clear all
+% clf
+% global Pstar cstar n maxcount M Q camax RT cI;
+% 
+% % for manually modifying beta = 0:0.1:1
+% beta = 1;
+% 
+% for i = 1:100
+%     disp("Iteration: "+ i)
+%     M = (0.25*cref*5.6)*i;
+%     
+%     setup_lung
+%     cvsolve
+%     outchecklung
+% end
+% 
+% % by modifying beta = 0:0.1:1, 
+% % I found out that when beta=1:0.1:0.7, M_max = 0.0330 
+% % and when beta = 0.8:1, M_max = 0.0220
+% M_max = [repelem(0.0330, 8), repelem(0.0220, 3)];
+% beta = 0:0.1:1;
+% figure(5)
+% plot(beta, M_max, 'o')
+% hold on
+% plot(beta, M_max, 'g')
+% xlabel("beta"); ylabel("maximum sustainable rate of oxygen consumption"); 
+% title("beta vs max sustainable rate of oxygen consumption")
 
 %%
 % [Not use] cannot figure out try and catch statement
@@ -192,11 +193,6 @@ for i = 1:1:300
     cvsolve
     outchecklung
 
-%     setup_lung
-%     try 
-%         cvsolve
-%         outchecklung
-
     Pv_list6(end+1) = Pv;       %mean partial pressure in venous blood
     Pabar_list6(end+1) = Pabar; %mean arterial oxygen partial pressure
     PAbar_list6(end+1) = PAbar; %mean alveolar oxygen partial pressure
@@ -205,13 +201,6 @@ for i = 1:1:300
     cabar_list6(end+1) = cabar; %mean arterial [oxygen] 
     cAbar_list6(end+1) = cAbar; %mean alevolar [oxygen]
 
-%         catch ME
-%             if strcmp(ME.message, 'M is too large')
-%                 fprintf("One cannot sustain with the normal resting rate of oxygen consumption when cref decreases to %0.5f with %d units of altitude increase \n", cref, i)
-%             else
-%                 rethrow(ME)
-%             end
-%     end
 end
 
 % run after the error message
@@ -345,8 +334,8 @@ clear all
 global Pstar cstar n maxcount M Q camax RT cI;
 
 %comment out cstar = cref --> change to cstar = 1.5*cref in setup_lung.m
-Pv_list9=[]; Pabar_list9=[]; PAbar_list9=[];
-cv_list9=[]; cabar_list9=[]; cAbar_list9=[];
+Pv_list9=[]; Pabar_list9=[]; PAbar_list9=[]; PI_list9=[];
+cv_list9=[]; cabar_list9=[]; cAbar_list9=[]; cI_list9=[];
 
 cref=0.2/(22.4*(310/273));
 beta = 1;
@@ -358,11 +347,13 @@ for i = 0:0.001:cref
     setup_lung
     cvsolve
     outchecklung
-
+    
+    PI_list9(end+1)=PI;         %inspired oxygen partial pressure
     Pv_list9(end+1) = Pv;       %mean partial pressure in venous blood
     Pabar_list9(end+1) = Pabar; %mean arterial oxygen partial pressure
     PAbar_list9(end+1) = PAbar; %mean alveolar oxygen partial pressure
 
+    cI_list9(end+1)=cI;         % inspired oxygen concentration
     cv_list9(end+1) = cv;       %[oxygen] in venous blood
     cabar_list9(end+1) = cabar; %mean arterial [oxygen] 
     cAbar_list9(end+1) = cAbar; %mean alevolar [oxygen]
@@ -371,24 +362,26 @@ end
 fprintf("One cannot sustain with the normal resting rate of oxygen consumption when cstar is decreased to %0.5f at beta = %0.1f\n", cstar, beta)
 
 figure(14)
-plot(cstar_list(1:end-1), Pv_list9)
+plot(cstar_list(1:end-1), PI_list9)
 hold on 
+plot(cstar_list(1:end-1), Pv_list9)
 plot(cstar_list(1:end-1), Pabar_list9)
 plot(cstar_list(1:end-1), PAbar_list9)
 hold off
 legend("Pv","Pabar","PAbar")
 xlabel("cstar"); ylabel("oxygen partial pressure");
-title("cstar vs oxygen partial pressure (beta = 0)")
+title("cstar vs oxygen partial pressure (beta = 1)")
 
 figure(15)
-plot(cstar_list(1:end-1), cv_list9)
+plot(cstar_list(1:end-1), cI_list9)
 hold on 
+plot(cstar_list(1:end-1), cv_list9)
 plot(cstar_list(1:end-1), cabar_list9)
 plot(cstar_list(1:end-1), cAbar_list9)
 hold off
 legend("cv",'cabar','cAbar')
 xlabel("cstar"); ylabel("oxygen concentration");
-title("cstar vs oxygen concentration (beta = 0)")
+title("cstar vs oxygen concentration (beta = 1)")
 
 %% Task 10
 
@@ -396,22 +389,12 @@ clear all
 clf
 global Pstar cstar n maxcount M Q camax RT cI;
 
-beta = 0;
-
 Pv_list10=[]; Pabar_list10=[]; PAbar_list10=[];
 cv_list10=[]; cabar_list10=[]; cAbar_list10=[];
 
 
-clear all
-clf
-global Pstar cstar n maxcount M Q camax RT cI;
-
-Pv_list10=[]; Pabar_list10=[]; PAbar_list10=[];
-cv_list10=[]; cabar_list10=[]; cAbar_list10=[];
-
-
-beta = 1; 
-cstar = 0.00236; % model individual with anemia
+beta = 0.5; 
+cstar = 0.00236; % to model individuals with anemia
 
 for i = 1:1:300
     cref=0.2/((22.4+i)*(310/273));
@@ -441,7 +424,7 @@ plot(PAbar_list10)
 hold off
 legend("Pv","Pabar","PAbar")
 xlabel("altitude"); ylabel("oxygen partial pressure");
-title("altitude vs oxygen partial pressure (beta = 1)")
+title("altitude vs oxygen partial pressure (beta = 0.5)")
 
 figure(17)
 plot(cv_list10)
@@ -451,4 +434,4 @@ plot(cAbar_list10)
 hold off
 legend("cv",'cabar','cAbar')
 xlabel("altitude"); ylabel("oxygen concentration");
-title("altitude vs oxygen concentration (beta = 1)")
+title("altitude vs oxygen concentration (beta = 0.5)")
